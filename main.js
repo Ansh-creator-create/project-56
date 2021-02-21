272395c1daaaea6dabf1a6977366d352c40efa00
Ansh-creator-create <79391724+Ansh-creator-create@users.noreply.github.com>
2021-02-21 10:37:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-function im4(){
-window.alert("isnature all around you when you took a look?")
+function im4() {
+    window.alert("is nature all around you when you took a look?")
 }
 
-             
-             
+function () {
+    window.alert("This one image makes you wanna go picnic doesnt it?")
+}
